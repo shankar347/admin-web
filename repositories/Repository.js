6e249d19a6624267b50f986527b3500ff159adb6
jeyarajsj <jeyaraj@bs-infotech.com>
2023-06-22 18:31:47 +0530

    changes

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 // export const baseurl = "https://medadmin.questioncloud.in/api";
-export const baseurl = "https://britishagro.com/api";
-//export const baseurl = "https://admin.jobslink.in/api/api/v1";
+export const baseurl = "https://britishagro.com/api/api";
 
+//export const baseurl = "http://192.168.1.170:5003/api";//JP
 
+ //export const baseurl = "http://192.168.1.165:4567/api/v1";
+//export const baseurl = "http://localhost:4567/api/v1";
 export const file = "http://localhost:4567/api/v1/user";
 export const apiUrl = baseurl;
 export const fileUpload = file;
