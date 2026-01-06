c56280ea2e824ea208f01ff3453cf7e8c57d1089
sivaaadi96 <sivaaadi96@gmail.com>
2026-01-06 17:41:42 +0530

    new changes

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseurl = "http://192.168.1.148:5000/api/v1";
-// export const baseurl = "https://courierapi.vercel.app/api/v1";
+// export const baseurl = "http://192.168.1.148:5000/api/v1";
+export const baseurl = "https://courierapi.vercel.app/api/v1";
 
 export const apiUrl = baseurl;
 
